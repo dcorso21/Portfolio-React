263447ca04df622cae80528918c4dd518c4a4daa
dcorso21 <david.l.corson@gmail.com>
2020-10-26 14:09:30 -0400

    test change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ch ch ch ch Changes
         </a>
       </header>
     </div>
