4094720da1b8aa0270e5dc7e985241169b68add6
dcorso21 <david.l.corson@gmail.com>
2020-10-26 17:39:30 -0400

    sass setup complete

---
+++ src/components/landing.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles/css/components/landing.css'
+
+export default class Landing extends Component {
+    render() {
+        return (
+            <section className= "landing-page">
+                Hi there!
+            </section>
+        )
+    }
+}
