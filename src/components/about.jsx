ec2a4cc43be991f44e91887ac00fafdf487f3700
dcorso21 <david.l.corson@gmail.com>
2020-10-28 13:37:08 -0400

    back to vertical layout

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,13 +6,13 @@ export default class About extends Component {
         return (
             <section id="about">
                 <div className="lay">
-                    <div className="title">
-                        <h1>About</h1>
-                    </div>
                     <div className="info">
                         Lorem ipsum, dolor sit amet consectetur adipisicing
                         elit. Iure deleniti odit eius nihil placeat, error totam
                         eos inventore iusto doloribus?
+                    </div>
+                    <div className="title">
+                        <h1>About</h1>
                     </div>
                 </div>
             </section>
