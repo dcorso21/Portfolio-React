ffea25fdbbe31615e82305abe9427708b6d78830
dcorso21 <david.l.corson@gmail.com>
2020-10-28 14:53:22 -0400

    about page and etc

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,13 +6,23 @@ export default class About extends Component {
         return (
             <section id="about">
                 <div className="lay">
-                    <div className="info">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Iure deleniti odit eius nihil placeat, error totam
-                        eos inventore iusto doloribus?
-                    </div>
                     <div className="title">
-                        <h1>About</h1>
+                        <h1>About Me</h1>
+                        {/* <div className="email">david.l.corson@gmail.com</div> */}
+                    </div>
+                    <div className="info">
+                        <p>
+                            My name is David Corson and for several years I have
+                            followed a career in Music Production and Sound
+                            Engineering. I have been lucky enough to work in
+                            world class studios in Chicago and Orlando, later
+                            becoming a teacher for Full Sail University. In late
+                            2019, after having done several pet projects with
+                            Python, I took a chance and decided to change career
+                            paths towards software engineering. For quite some
+                            time now I have been creating a complete algorithmic
+                            trading bot. Thank you for visiting my site!
+                        </p>
                     </div>
                 </div>
             </section>
